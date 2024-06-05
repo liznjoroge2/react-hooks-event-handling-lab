a0cb27f0593cc69fc3c75a0fc836ed0d942757fc
enjoroge <njrogeelizabeth53@gmail.com>
2024-06-05 15:17:52 +0300

    commit

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,23 @@
-// Code Keypad Component Here
+import React, { useState } from 'react';
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+function Keypad() {
+  const [password, setPassword] = useState('');
+
+  const handleChange = (event) => {
+    console.log('Entering password...');
+    setPassword(event.target.value);
+  };
+
+  return (
+    <div>
+      <input
+        type="password"
+        value={password}
+        onChange={handleChange}
+        placeholder="Enter password"
+      />
+    </div>
+  );
 }
 
 export default Keypad;
